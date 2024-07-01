2c99086e1326af14c4075c1b0dfb3e8ccb3c55d7
JaniAhl <jani.n@icloud.com>
2024-07-01 23:34:19 +0300

    Button now counts BMI and shows it under the button, also added a text under the number to show explanation to number.

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,40 @@
+import { useState } from 'react'
+import React from 'react'
 import './App.css'
 
-
 function App() {
+
+  const [weight, setWeight] = useState<number | ''>('');
+  const [height, setHeight] = useState<number | ''>('');
+  const [bmi, setBmi] = useState<number | null>(null);
+  const [bmiText, setBmiText] = useState<string>('');
+
+
+  const calculateBmi = () => {
+    if (height && weight) {
+      const heightInMeters = height / 100;
+      const bmiValue = weight / (heightInMeters * heightInMeters);
+      setBmi(bmiValue);
+
+      let bmiText = '';
+
+      if (bmiValue < 17) {
+        bmiText = 'Vaarallinen aliravitsemus';
+      } else if (bmiValue < 18.5) {
+        bmiText = 'Alipaino';
+      } else if (bmiValue < 25) {
+        bmiText = 'Normaalipaino';
+      } else if (bmiValue < 30) {
+        bmiText = 'Ylipaino';
+      } else if (bmiValue < 35) {
+        bmiText = 'Merkittävä ylipaino';
+      } else if (bmiValue < 40) {
+        bmiText = 'Sairaalloinen ylipaino';
+      }
+
+      setBmiText(bmiText);
+    }
+  };
 
   return (
     <>
@@ -14,17 +47,39 @@ function App() {
 
           <div className='heightcontainer'>
             <span>Pituus (CM):</span>
-            <input type='text' id='heightInput'/>
+            <input 
+              type='text' 
+              id='heightInput'
+              value={height}
+              onChange={(e) => setHeight(parseFloat(e.target.value) || '')}
+            />
           </div>
 
           <div className='weightcontainer'>
             <span>Paino (KG):</span>
-            <input type='text' id='weightInput' />
+            <input 
+              type='text' 
+              id='weightInput' 
+              value={weight}
+              onChange={(e) => setWeight(parseFloat(e.target.value) || '')}
+              />
           </div> 
 
           <div className='buttoncontainer'>
-            <button>Laske BMI</button>
+            <button onClick={calculateBmi}>Laske BMI</button>
           </div>
+
+          {bmi !== null && (
+            <div className='bmivalue'>
+              <span>BMI:</span> {bmi.toFixed(2)}
+            </div>
+          )}
+
+          {bmiText && (
+            <div className='bmiText'>
+              <span>{bmiText}</span>
+            </div>
+          )}
 
         </div>
       
